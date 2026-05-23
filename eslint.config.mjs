d3e027c869c9fe8a91467baa85148d151d87c557
Al Ameen S <alameensameen77@gmail.com>
2026-05-23 16:46:11 +0530

    chore : added eslint in project

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+
+    languageOptions: {
+      globals: globals.node,
+      parser: tseslint.parser,
+    },
+
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+]);
